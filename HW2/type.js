b81da31395b9e38a35483c0e38dbd5d714cad150
Guritanea <73936476+Guritanea@users.noreply.github.com>
2021-02-07 10:14:13 +0200

    Update type.js

--- HW2/type.js
+++ HW2/type.js
@@ -1,9 +1,1 @@
-module.exports.isNumber=function(a){
-  // let a=[]
-  //   data.filter(v=>typeof(v)!="string").forEach(v=>{a.push(v)})
-  //   return a
-  if (typeof a === 'number') {
-    return a
-  }
-  }
-
+module.exports.isNumber=(a => (typeof a) == 'number')
