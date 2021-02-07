4b7b3899f6b6fc9b80025464027ec5e5c24b5c4c
Guritanea <73936476+Guritanea@users.noreply.github.com>
2021-02-07 10:14:56 +0200

    Update app.js

--- HW2/app.js
+++ HW2/app.js
@@ -4,10 +4,10 @@ let config = require("./temperatures.json")
 
 const {div,mul,add,sub} = require("./math")
 
-// let isNumber = require("./type")
+let isNumber = require("./type")
 let s=0
 let n=0
-config.forEach(v=>typeof v=== 'number'? n=add(n,v):s=add(s,1))
+config.forEach(v=>isNumber(v)? n=add(n,v):s=add(s,1))
 
 console.log("Average temperature=", div(n,(config.length-s)))
 console.log("Error=", div(mul(s,100),config.length)+"%")
