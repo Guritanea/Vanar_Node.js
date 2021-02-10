2f6b931a1daf9ad8b6b7aab353650ee7abd7c7df
Guritanea <73936476+Guritanea@users.noreply.github.com>
2021-02-10 09:01:25 +0200

    Update app-filter-numbers.js

--- HW1/app-filter-numbers.js
+++ HW1/app-filter-numbers.js
@@ -1,16 +1,12 @@
 data = [10, 0.5, -2, "+100", -1.5, 25, "50", "0.5"]
 
 function filterFormatted(data){
-    // 1 вариант реализации
-    // let a= data.filter(item=>typeof(item)!="string")
-    // console.log(a)
-//-----------------------------------
-    //2вариант реализации
-   let a=[]
-    data.filter(v=>typeof(v)!="string").forEach(v=>{a.push(v)})
-    console.log(a)
+let a=[]
+data.forEach(v=>typeof(v)=="number"?a.push(v):false)
+//console.log(a)
+    return a
 }
- filterFormatted(data)
+filterFormatted(data)
 
 
 
